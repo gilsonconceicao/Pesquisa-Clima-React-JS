e3b1df211dff127608a0d6a949a87386562d7605
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2022-10-14 02:05:10 -0300

    project search whather

--- src/components/SearchTemp/SearchTemp.jsx
+++ src/components/SearchTemp/SearchTemp.jsx
@@ -6,10 +6,10 @@ export const SearchTemp = () => {
   const [city, setCity] = useState('');
   const [dataTemp, setData] = useState(null);
   const [dataGeral, setDataGeral] = useState(null);
-  const [show, setShow] = useState(false); 
-  
+  const [show, setShow] = useState(false);
+  const [hours, setHours] = useState('');
   //get data store 
-  const dataStore = JSON.parse(localStorage.getItem('@dataStoreUser')); 
+  const dataStore = JSON.parse(localStorage.getItem('@dataStoreUser'));
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=32e98030c971e83daa9b089454f9fae2&units=metric&lang=pt-br`;
 
@@ -18,18 +18,28 @@ export const SearchTemp = () => {
       const get = await fetch(url)
         .then(response => response.json())
         .then((data) => {
+          const currenHours = new Date().getHours();
+          if (currenHours >= 5 && currenHours < 12) {
+            setHours('Bom dia')
+          } else if (currenHours >= 12 && currenHours < 18) {
+            setHours('Boa tarde')
+          } else {
+            setHours('Boa noite')
+          }
           setData(data.main);
           setDataGeral(data);
         })
     }
     GetDataApiTemperature();
-  }, [city])
+  }, [city]);
 
-  const handleSearchForNameCity = () => setShow(true); 
+  const handleSearchForNameCity = () => {
+    setShow(true)
+  };
 
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Bem vindo, {dataStore.firstName +' '+ dataStore.lastName}.</h2>
+      <h2 className={styles.title}>{hours}, {dataStore.firstName + ' ' + dataStore.lastName}.</h2>
       <p className={styles.paragraph}>Busque por clima atual da sua cidade, do seu bairro ou ateé masmo um país.</p>
       <form onSubmit={handleSearchForNameCity} className={styles.flex_search}>
         <input
